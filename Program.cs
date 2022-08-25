65054e46476bdb9ded0023ee47a29e66a47b94c1
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-25 23:49:54 +0300

    поиск третьей цифры

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n>99 ? n.ToString()[2] : "-" );
+   if (n < 99)  Console.WriteLine("Третьей цифры нет");
